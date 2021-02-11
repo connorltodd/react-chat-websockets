0cfd1a1eb093be6dba9784fcb962582c2bed9e8c
Connor Todd <connortodd0304@gmail.com>
2021-02-11 11:25:35 +0000

    completed web sockets lesson

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import socketIOClient from "socket.io-client";
+
+function App() {
+  const [messageList, setMessageList] = useState([]);
+  const [nickName, setNickName] = useState("");
+  const [newMessageText, setNewMessageText] = useState("");
+  const [socket, setSocket] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("messageFromClient", {
+      text: newMessageText,
+      author: nickName,
+    });
+  };
+
+  React.useEffect(() => {
+    const socket = socketIOClient("http://localhost:5000");
+    setSocket(socket);
+
+    // receiving the event initialMessageList
+    socket.on("initialMessageList", (messages) => {
+      setMessageList(messages);
+    });
+
+    socket.on("messageFromServer", (newMessage) =>
+      setMessageList((messageList) => [...messageList, newMessage])
+    );
+
+    return () => socket.disconnect();
+  }, []);
+
+  return (
+    <div className="App">
+      <h2>Messages</h2>
+      {messageList.map((message) => {
+        return (
+          <div key={message.id}>
+            {message.author} : {message.text}
+          </div>
+        );
+      })}
+
+      <form onSubmit={handleSubmit}>
+        <h2>New Message</h2>
+        <input
+          type="text"
+          name="author"
+          placeholder="nickname"
+          value={nickName}
+          required
+          onChange={(e) => setNickName(e.target.value)}
+        />
+        <input
+          type="text"
+          name="messageContent"
+          placeholder="message"
+          value={newMessageText}
+          required
+          onChange={(e) => setNewMessageText(e.target.value)}
+        />
+        <input type="submit" value="send" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
